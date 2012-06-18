1cce2dcdcafdc9d80044c64a7607598e166bd4d9
Robert Dallas Gray <mail@robertdallasgray.com>
2012-06-18 17:47:17 +0100

    Added jsonp support.

--- archie_canmore.rb
+++ archie_canmore.rb
@@ -7,14 +7,18 @@ require 'nokogiri'
 require './request.rb'
 
 get '/' do 
-  content_type :json
-  [].to_json
 end
 
 get '/:lat/:long/:rad' do
-  content_type :json
-  res = Canmore::Request.images_for_location(:lat => params[:lat].to_f, 
-                                             :long => params[:long].to_f, 
-                                             :rad => params[:rad].to_i)
-  res.to_json
+  res = Canmore::Request.images_for_location(:lat => params[:lat].to_f, :long => params[:long].to_f, :rad => params[:rad].to_i)
+  json = res.to_json
+
+  if params[:callback]
+    json = "#{params[:callback]}(#{json});"
+    content_type 'text/javascript'
+  else
+    content_type 'json'
+  end
+
+  json
 end
