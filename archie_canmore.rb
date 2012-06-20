1c40136416f143865dadf393b4ef1a1f6db7f718
Robert Dallas Gray <mail@robertdallasgray.com>
2012-06-20 13:40:15 +0100

    Using six or seven digit rels.

--- archie_canmore.rb
+++ archie_canmore.rb
@@ -7,14 +7,14 @@ get '/' do
 end
 
 get '/detail_rels_for/:lat/:long/:rad' do
-  res = Canmore::Request.detail_rels_for(params[:rad].to_i, :lat => params[:lat].to_f, :long => params[:long].to_f)
+  res = Canmore::Request.new().detail_rels_for(params[:rad].to_i, :lat => params[:lat].to_f, :long => params[:long].to_f)
   
   content_type 'text/javascript'
   "#{params[:callback]}(#{res.to_json});"
 end
 
 get '/details_for/:rel' do
-  res = Canmore::Request.details_for params[:rel]
+  res = Canmore::Request.new().details_for params[:rel]
   
   content_type 'text/javascript'
   "#{params[:callback]}(#{res.to_json});"
--- lib/canmore/request.rb
+++ lib/canmore/request.rb
@@ -3,32 +3,34 @@ require 'silva'
 require_relative 'parser'
 
 module Canmore
-  module Request
+  class Request
     CANMORE_URL = "http://canmore.rcahms.gov.uk"
     SEARCH_URL = "/en/results/"
-    SEARCH_DEFAULT_PARAMS={ :site_country => 1, :image_only_chk => 1, :submit => 'search'}    
+    SEARCH_DEFAULT_PARAMS = { :site_country => 1, :image_only_chk => 1, :submit => 'search'}
+    DETAIL_URL = "/en/site/:rel/details/"
     THUMB_URL = "/images/m/"
 
-    def self.get_html(url = CANMORE_URL, params = nil, client = HTTParty)
-      client.get(CANMORE_URL + url, :query => params).body
-    end    
-    
-    def self.search_by_location(radius, location)
-      gridref = Silva::Location.from(:wgs84, :lat => location[:lat], :long => location[:long]).to(:gridref)
-      params = SEARCH_DEFAULT_PARAMS.merge({ :ngr => gridref, :locat_xy_radius_m => radius })
-      get_html(SEARCH_URL, params)
+    def initialize(client = HTTParty)
+      @client = client
     end
 
-    def self.detail_rels_for(radius, location)
+    ##
+    # Return an array of six- or seven-digit numbers which can be used to find individual site records on Canmore,
+    # given a lat/long location and a radius in which to search.
+    #
+    def detail_rels_for(radius, location)
       html = search_by_location(radius, location)
       image_details = []
       detail_links = Canmore::Parser::Search.new(html).detail_links
-      six_digit_link = /site\/([0-9]{6})\//
-      detail_links.select {|link| link.to_s =~ six_digit_link}.map {|link| link.to_s.match(six_digit_link)[1]}
+      six_or_seven_digits = /site\/([0-9]{6,7})\//
+      detail_links.select {|link| link.to_s =~ six_or_seven_digits}.map {|link| link.to_s.match(six_or_seven_digits)[1]}
     end
 
-    def self.details_for(rel)
-      link = "/en/site/#{rel}/details/"
+    ##
+    # Return a hash of details on a given site, given a six/seven-digit rel to search on.
+    #
+    def details_for(rel)
+      link = DETAIL_URL.sub(/:rel/, rel)
       details = { :site_link => CANMORE_URL + link }
       html = get_html(link)
       parser = Canmore::Parser::Detail.new(html)
@@ -40,6 +42,19 @@ module Canmore
       details[:thumb_link] = CANMORE_URL + THUMB_URL + image_rels.first + '/'
       details[:no_images] = image_rels.count
       details
+    end
+
+    private
+
+    def get_html(url, params = nil)
+      response = @client.get(CANMORE_URL + url, :query => params)
+      response.body
+    end
+    
+    def search_by_location(radius, location)
+      gridref = Silva::Location.from(:wgs84, :lat => location[:lat], :long => location[:long]).to(:gridref)
+      params = SEARCH_DEFAULT_PARAMS.merge({ :ngr => gridref.to_s, :locat_xy_radius_m => radius })
+      get_html(SEARCH_URL, params)
     end
   end
 end
