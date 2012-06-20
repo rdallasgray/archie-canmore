729c1b705fa54620bf6ee131b6381db9d9a2ce31
Robert Dallas Gray <mail@robertdallasgray.com>
2012-06-20 15:22:07 +0100

    Ensure a json response is returned even if there are exceptions.

--- archie_canmore.rb
+++ archie_canmore.rb
@@ -7,15 +7,21 @@ get '/' do
 end
 
 get '/detail_rels_for/:lat/:long/:rad' do
-  res = Canmore::Request.new().detail_rels_for(params[:rad].to_i, :lat => params[:lat].to_f, :long => params[:long].to_f)
-  
-  content_type 'text/javascript'
-  "#{params[:callback]}(#{res.to_json});"
+  begin
+    res = Canmore::Request.new().detail_rels_for(params[:rad].to_i, :lat => params[:lat].to_f, :long => params[:long].to_f)
+  rescue
+    res = []
+  end
+    content_type 'text/javascript'
+    "#{params[:callback]}(#{res.to_json});"
 end
 
 get '/details_for/:rel' do
-  res = Canmore::Request.new().details_for params[:rel]
-  
-  content_type 'text/javascript'
-  "#{params[:callback]}(#{res.to_json});"
+  begin
+    res = Canmore::Request.new().details_for params[:rel]
+  rescue
+    res = []
+  end
+    content_type 'text/javascript'
+    "#{params[:callback]}(#{res.to_json});"
 end
