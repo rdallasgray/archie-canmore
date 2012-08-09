883fcc9e8e461c7da2abd1671ea86a84b57108e0
Robert Dallas Gray <mail@robertdallasgray.com>
2012-08-09 15:56:49 +0100

    Added repor_user_action and user_actions to record evaluation events.

---
+++ lib/canmore/model/action_report.rb
@@ -0,0 +1,21 @@
+require 'data_mapper'
+DataMapper.setup(:default, ENV['HEROKU_POSTGRESQL_VIOLET_URL'] || 'postgres://localhost/canmore')
+
+
+module Canmore
+  module Model
+    class ActionReport
+      include DataMapper::Resource
+      
+      property :id, Serial
+      property :action, String
+      property :time, DateTime
+      property :device_id, String
+      property :lat, Float
+      property :long, Float
+    end
+  end
+end
+  
+DataMapper.finalize
+DataMapper.auto_upgrade!
