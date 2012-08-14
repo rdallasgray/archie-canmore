4af295cbf3aa16de772e5b42008a1b1f7529686d
Robert Dallas Gray <mail@robertdallasgray.com>
2012-08-14 13:12:31 +0100

    Removed architect files -- now only maintaining web service in this repo.

--- archie_canmore.rb
+++ archie_canmore.rb
@@ -3,21 +3,6 @@ require 'sinatra'
 require 'json'
 require_relative 'lib/canmore'
 
-get '/' do 
-end
-
-get '/dev/test' do
-  send_file File.expand_path('archie_test.html', 'architect/')
-end
-
-get '/dev/js/:file' do
-  send_file File.expand_path(params[:file], 'architect/lib/')
-end
-
-get '/dev/css/:file' do
-  send_file File.expand_path(params[:file], 'public/stylesheets/')
-end
-
 get '/site_ids_for_location/:lat/:long/:rad' do
   begin
     res = Canmore::Request.new().site_ids_for_location(params[:rad].to_i, :lat => params[:lat].to_f, :long => params[:long].to_f)
